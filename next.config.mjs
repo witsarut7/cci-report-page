db4c76beae1c5abb97c58ff8264ad1efac1fcd61
witsarut7 <tesinw@gmail.com>
2024-05-03 15:14:30 +0700

    :sparkles: Refactor code

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "development";
-const nextConfig = {
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_SERVICE_URL : undefined,
-};
+const nextConfig = {};
 
 export default nextConfig;
