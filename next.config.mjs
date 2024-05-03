5889b44075707103a88776561bbed35944d53054
witsarut7 <tesinw@gmail.com>
2024-05-03 14:31:11 +0700

    edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "development";
+const nextConfig = {
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_SERVICE_URL : undefined,
+};
 
 export default nextConfig;
