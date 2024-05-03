97ed8916bd4f3b61c050cb47dc27547c8478161a
witsarut7 <tesinw@gmail.com>
2024-05-03 16:06:17 +0700

    add option experimenta in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "pnd.cciofficial.com",
+        "61.19.247.202:4004",
+        "http://61.19.247.202:4004",
+        "https://pnd.cciofficial.com",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
